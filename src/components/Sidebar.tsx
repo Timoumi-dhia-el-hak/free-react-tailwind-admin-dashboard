989ed70db1ca22d0bbfb894e8c167fb2908b663f
dhia el hak timoumi <timoumi.dhia88@gmail.com>
2024-02-14 21:08:39 +0100

    fix sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,9 +2,6 @@ import React, { useEffect, useRef, useState } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import Logo from '../images/logo/logo.svg';
 import SidebarLinkGroup from './SidebarLinkGroup';
-import { FaRegUser } from "react-icons/fa";
-import { FaRegFile } from "react-icons/fa6";
-import { FaReact } from "react-icons/fa";
 interface SidebarProps {
   sidebarOpen: boolean;
   setSidebarOpen: (arg: boolean) => void;
@@ -13,8 +10,7 @@ interface SidebarProps {
 const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
   const location = useLocation();
   const { pathname } = location;
-  const [sideopen, setOpen]=useState(true);
-  console.log('side',sideopen)
+
   const trigger = useRef<any>(null);
   const sidebar = useRef<any>(null);
 
@@ -49,7 +45,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
     return () => document.removeEventListener('keydown', keyHandler);
   });
 
- useEffect(() => {
+  useEffect(() => {
     localStorage.setItem('sidebar-expanded', sidebarExpanded.toString());
     if (sidebarExpanded) {
       document.querySelector('body')?.classList.add('sidebar-expanded');
@@ -61,41 +57,42 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
   return (
     <aside
       ref={sidebar}
-      className={` left-0 top-0 z-9999 flex h-screen flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0 ${ !sideopen ? 'translate-x-full' : ' -translate-x-0'}`} >
+      className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0 ${
+        sidebarOpen ? 'translate-x-0' : '-translate-x-full'
+      }`}
+    >
       {/* <!-- SIDEBAR HEADER --> */}
-      <div className={`relative flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5 ${ !sideopen ? 'w-30' : ' w-72'} `}>
+      <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
         <NavLink to="/">
-          <span className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 text-4xl text-bodydark1 duration-300 ease-in-out dark:hover:bg-meta-4 ${ !sideopen && "scale-0"} `}>ProPanel</span>
-          <span className='text-2xl block float-left'> 
-          <FaReact alt="Logo"className={`${ sideopen && "scale-0"}`}size={40}/></span>
+        <span className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 text-4xl text-bodydark1 duration-300 ease-in-out dark:hover:bg-meta-4 `}>ProPanel</span>
+        
         </NavLink>
 
         <button
           ref={trigger}
-          onClick={() => setOpen(!sideopen)}
+          onClick={() => setSidebarOpen(!sidebarOpen)}
           aria-controls="sidebar"
-          aria-expanded={sideopen}
-          className={'flex items-center justify-center p-0.5 bg-gray-800 focus:outline-none  focus:ring-gray-500 hover:bg-graydark dark:hover:bg-meta-4 absolute right-3 top-8 '}>
+          aria-expanded={sidebarOpen}
+          className="block lg:hidden"
+        >
           <svg
-            className={`fill-current ${ !sideopen && "rotate-180"}`} 
+            className="fill-current"
             width="20"
             height="18"
             viewBox="0 0 20 18"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
-            
           >
             <path
               d="M19 8.175H2.98748L9.36248 1.6875C9.69998 1.35 9.69998 0.825 9.36248 0.4875C9.02498 0.15 8.49998 0.15 8.16248 0.4875L0.399976 8.3625C0.0624756 8.7 0.0624756 9.225 0.399976 9.5625L8.16248 17.4375C8.31248 17.5875 8.53748 17.7 8.76248 17.7C8.98748 17.7 9.17498 17.625 9.36248 17.475C9.69998 17.1375 9.69998 16.6125 9.36248 16.275L3.02498 9.8625H19C19.45 9.8625 19.825 9.4875 19.825 9.0375C19.825 8.55 19.45 8.175 19 8.175Z"
               fill=""
             />
           </svg>
-            </button>
-
+        </button>
       </div>
       {/* <!-- SIDEBAR HEADER --> */}
 
-      <div className={`no-scrollbar flex flex-col overflow-y-auto duration-300 ease-linear ${ !sideopen ? 'w-30' : ' w-72'} `}>
+      <div className="no-scrollbar flex flex-col overflow-y-auto duration-300 ease-linear">
         {/* <!-- Sidebar Menu --> */}
         <nav className="mt-5 py-4 px-4 lg:mt-9 lg:px-6">
           {/* <!-- Menu Group --> */}
@@ -110,8 +107,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                 {(handleClick, open) => {
                   return (
                     <React.Fragment>
-                      
-                     <NavLink
+                      <NavLink
                         to="#"
                         className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
                           (pathname === '/auth' || pathname.includes('auth')) &&
@@ -121,13 +117,11 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                           e.preventDefault();
                           sidebarExpanded
                             ? handleClick()
-                            : setSidebarExpanded(false);
+                            : setSidebarExpanded(true);
                         }}
-                        
                       >
-                        <span className='text-2xl block float-left'> 
                         <svg
-                          className="fill-current "
+                          className="fill-current"
                           width="18"
                           height="19"
                           viewBox="0 0 18 19"
@@ -154,9 +148,8 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                               />
                             </clipPath>
                           </defs>
-                        </svg></span>
-                       
-                        <h1 className={`${ !sideopen ? 'scale-0' : ' scale-1'}`}>Authentication</h1>
+                        </svg>
+                        Authentication
                         <svg
                           className={`absolute right-4 top-1/2 -translate-y-1/2 fill-current ${
                             open && 'rotate-180'
@@ -220,10 +213,32 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                     pathname.includes('users') && 'bg-graydark dark:bg-meta-4'
                   }`}
                 >
-                  <span className='text-2xl block float-left'> 
-                 <FaRegUser />
-                 </span>
-                 <h1 className={`${ !sideopen ? 'scale-0' : ' scale-1'}`}>Users</h1>
+                  <svg
+                    className="fill-current"
+                    width="18"
+                    height="19"
+                    viewBox="0 0 18 19"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g clipPath="url(#clip0_130_9756)">
+                      <path
+                        d="M15.7501 0.55835H2.2501C1.29385 0.55835 0.506348 1.34585 0.506348 2.3021V15.8021C0.506348 16.7584 1.29385 17.574 2.27822 17.574H15.7782C16.7345 17.574 17.5501 16.7865 17.5501 15.8021V2.3021C17.522 1.34585 16.7063 0.55835 15.7501 0.55835ZM6.69385 10.599V6.4646H11.3063V10.5709H6.69385V10.599ZM11.3063 11.8646V16.3083H6.69385V11.8646H11.3063ZM1.77197 6.4646H5.45635V10.5709H1.77197V6.4646ZM12.572 6.4646H16.2563V10.5709H12.572V6.4646ZM2.2501 1.82397H15.7501C16.0313 1.82397 16.2563 2.04897 16.2563 2.33022V5.2271H1.77197V2.3021C1.77197 2.02085 1.96885 1.82397 2.2501 1.82397ZM1.77197 15.8021V11.8646H5.45635V16.3083H2.2501C1.96885 16.3083 1.77197 16.0834 1.77197 15.8021ZM15.7501 16.3083H12.572V11.8646H16.2563V15.8021C16.2563 16.0834 16.0313 16.3083 15.7501 16.3083Z"
+                        fill=""
+                      />
+                    </g>
+                    <defs>
+                      <clipPath id="clip0_130_9756">
+                        <rect
+                          width="18"
+                          height="18"
+                          fill="white"
+                          transform="translate(0 0.052124)"
+                        />
+                      </clipPath>
+                    </defs>
+                  </svg>
+                 Users
                 </NavLink>
               </li>
               {/* <!-- Menu Item  Users--> */}
@@ -236,10 +251,32 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                     pathname.includes('Files Manager') && 'bg-graydark dark:bg-meta-4'
                   }`}
                 >
-                   <span className='text-2xl block float-left'> 
-                   <FaRegFile />
-                 </span>
-                  <h1 className={`${ !sideopen ? 'scale-0' : ' scale-1'}`}>File Manager</h1> 
+                  <svg
+                    className="fill-current"
+                    width="18"
+                    height="19"
+                    viewBox="0 0 18 19"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g clipPath="url(#clip0_130_9756)">
+                      <path
+                        d="M15.7501 0.55835H2.2501C1.29385 0.55835 0.506348 1.34585 0.506348 2.3021V15.8021C0.506348 16.7584 1.29385 17.574 2.27822 17.574H15.7782C16.7345 17.574 17.5501 16.7865 17.5501 15.8021V2.3021C17.522 1.34585 16.7063 0.55835 15.7501 0.55835ZM6.69385 10.599V6.4646H11.3063V10.5709H6.69385V10.599ZM11.3063 11.8646V16.3083H6.69385V11.8646H11.3063ZM1.77197 6.4646H5.45635V10.5709H1.77197V6.4646ZM12.572 6.4646H16.2563V10.5709H12.572V6.4646ZM2.2501 1.82397H15.7501C16.0313 1.82397 16.2563 2.04897 16.2563 2.33022V5.2271H1.77197V2.3021C1.77197 2.02085 1.96885 1.82397 2.2501 1.82397ZM1.77197 15.8021V11.8646H5.45635V16.3083H2.2501C1.96885 16.3083 1.77197 16.0834 1.77197 15.8021ZM15.7501 16.3083H12.572V11.8646H16.2563V15.8021C16.2563 16.0834 16.0313 16.3083 15.7501 16.3083Z"
+                        fill=""
+                      />
+                    </g>
+                    <defs>
+                      <clipPath id="clip0_130_9756">
+                        <rect
+                          width="18"
+                          height="18"
+                          fill="white"
+                          transform="translate(0 0.052124)"
+                        />
+                      </clipPath>
+                    </defs>
+                  </svg>
+                 File Manager
                 </NavLink>
               </li>
               {/* <!-- Menu Item  File Manager --> */}
@@ -247,7 +284,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
               <li>
               <SidebarLinkGroup
                   activeCondition={
-                    pathname === '/setting' || pathname.includes('settting')
+                    pathname === '/auth' || pathname.includes('auth')
                   }
                 >
                   {(handleClick, open) => {
@@ -272,7 +309,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                       : setSidebarExpanded(true);
                   }}
                 >
-                <span className='text-2xl block float-left'> 
+                
                   <svg
                     className="fill-current"
                     width="18"
@@ -302,8 +339,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                       </clipPath>
                     </defs>
                   </svg>
-                  </span>
-                  <h1 className={`${ !sideopen ? 'scale-0' : ' scale-1'}`}>Settings</h1> 
+                  Settings
                   <svg
                           className={`absolute right-4 top-1/2 -translate-y-1/2 fill-current ${
                             open && 'rotate-180'
@@ -375,9 +411,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
               {/* <!-- Menu Item Settings --> */}
             </ul>
           </div>
-               
-          <div className={`${ !sideopen && "scale-0"}`}>
-            <h3 className="mb-4 ml-4 text-sm font-semibold text-bodydark2">
+                     <h3 className="mb-4 ml-4 text-sm font-semibold text-bodydark2">
               MENU
             </h3>
 
@@ -723,10 +757,10 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
               </li>
               {/* <!-- Menu Item Settings --> */}
             </ul>
-          </div>
+          
 
           {/* <!-- Others Group --> */}
-          <div className={`${ !sideopen && "scale-0"}`}>
+       
             <h3 className="mb-4 ml-4 text-sm font-semibold text-bodydark2">
               OTHERS
             </h3>
@@ -992,7 +1026,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
               </SidebarLinkGroup>
               {/* <!-- Menu Item Auth Pages --> */}
             </ul>
-          </div>
+          
         </nav>
         {/* <!-- Sidebar Menu --> */}
       </div>
