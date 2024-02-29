bd34ed2d58daceb43588ae255cb5303a3d237e4c
dhia el hak timoumi <timoumi.dhia88@gmail.com>
2024-02-29 09:01:34 +0100

    add reduxjs/toolkit

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Routes,Route, BrowserRouter} from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import './satoshi.css';
 import { PublicClientApplication } from '@azure/msal-browser';
 import { msalConfig } from './auth/authConfig';
 import { MsalProvider } from '@azure/msal-react';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 const msalInstance = new PublicClientApplication(msalConfig);
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-    <MsalProvider instance={msalInstance}>
-        <App />
-    </MsalProvider>
+      <MsalProvider instance={msalInstance}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </MsalProvider>
     </BrowserRouter>
-    
-  </React.StrictMode>
+  </React.StrictMode>,
 );
